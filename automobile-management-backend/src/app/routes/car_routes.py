bd45111998ba76775ecc17764beccbd999f458b3
szemerics <szemericsdominik@gmail.com>
2025-07-20 15:00:53 +0200

    Implemented update_car and delete_car

--- automobile-management-backend/src/app/routes/car_routes.py
+++ automobile-management-backend/src/app/routes/car_routes.py
@@ -6,16 +6,20 @@ router = APIRouter()
 
 @router.get('/get_cars')
 async def get_cars():
-   result = await car_service.get_all_cars()
-
-   return result
+   return await car_service.get_all_cars()
 
 @router.get('/{car_id}')
 async def get_car_by_id(car_id: str):
-   result = await car_service.get_car_by_id(car_id)
-
-   return result
+   return await car_service.get_car_by_id(car_id)
 
 @router.post('/')
 async def create_car(car_data: CreateCar):
    return await car_service.create_car(car_data)
+
+@router.delete('/')
+async def delete_car(car_id: str):
+   return await car_service.delete_car(car_id)
+
+@router.put('/{car_id}')
+async def update_car(car_id:str, car_data:CreateCar):
+   return await car_service.update_car(car_id, car_data)
--- automobile-management-backend/src/app/services/car_service.py
+++ automobile-management-backend/src/app/services/car_service.py
@@ -24,3 +24,36 @@ async def create_car(car_data: CreateCar):
     await engine.save(car)
 
     return car
+
+async def delete_car(car_id: str):
+    car = await engine.find_one(Car, Car.id == ObjectId(car_id))
+
+    if not car:
+        return {"error": "Car not found"}
+    
+    await engine.delete(car)
+    return {"message": "Car deleted successfully"}
+
+async def update_car(car_id: str, car_data: CreateCar):
+    car = await engine.find_one(Car, Car.id == ObjectId(car_id))
+
+    if not car:
+        return {"error": "Car not found"}
+    
+    car.brand = car_data.brand
+    car.model = car_data.model
+    car.year = car_data.year
+    car.license_plate = car_data.license_plate
+    car.price = car_data.price
+    car.mileage = car_data.mileage
+    car.fuel_type = car_data.fuel_type
+    car.transimission = car_data.transimission
+    car.engine_power = car_data.engine_power
+    car.is_sold = car_data.is_sold
+    car.purchase_date = car_data.purchase_date
+    car.sale_date = car_data.sale_date
+    car.ads = car_data.ads
+
+    await engine.save(car)
+    
+    return {"message": "Car updated successfully"}
